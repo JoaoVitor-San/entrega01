cc51d3ed31a08c2bb2787cb2bc805f31429097d6
João Vitor Santana <joao.sant.dev@gmail.com>
2024-12-19 23:08:09 -0300

    continuação

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
 let digitadoPeloUsuario = prompt("Seu ano de nascimento:");
 let anoAtual = 2024; 
 let idade = anoAtual - digitadoPeloUsuario; 
+
+document.getElementById("resultado").innerText = "Sua idade é: " + idade;
